4db343e8f1a5ad6e737a7fde03634ee7e4b6385e
Jonathan Forscher <jonforscher@gmail.com>
2025-10-20 00:15:08 -0400

    Back to basics

--- scripts/update-pantry.js
+++
@@ -1,9 +0,0 @@
-import { fetchPastas } from '../src/pasta.js';
-import * as fs from 'node:fs';
-
-const pastas = fetchPastas();
-const json = JSON.stringify(pastas, null, 2);
-
-fs.writeFile('./data/pastas.json', json, (err) => {
-	console.error(err);
-});
